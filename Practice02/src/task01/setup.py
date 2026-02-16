b8f38d89a11e0ac35ddf17963e2b67079c6eaaf6
Artem Ivanov <art.ivanov@jetbrains.com>
2026-02-16 16:51:46 +0200

    Task 1

---
+++ Practice02/src/task01/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'task01'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/task01.launch']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='student',
+    maintainer_email='todo@todo.com',
+    description='Task 01 - String message receiver node',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'receiver = task01.receiver:main',
+        ],
+    },
+)
